985ea9c075825eadf6a36f7ae70f2a291852a602
Svenlaa <sven.lammertink@hotmail.com>
2022-08-12 13:05:44 +0000

    disable ESLint rules

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client'
 import { env } from '../../env/server.mjs'
 
 declare global {
+  // eslint-disable-next-line no-unused-vars
   let prisma: PrismaClient | undefined
 }
 

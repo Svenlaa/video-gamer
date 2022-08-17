d03f09b056d23457ce80cb12bc4ee85724286e13
Svenlaa <sven.lammertink@hotmail.com>
2022-08-17 12:41:05 +0200

    Split DB_URL, edit some configs

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,7 +6,12 @@ import { z } from 'zod'
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_HOST: z.string(),
+  DB_PORT: z.string(),
+  DB_NAME: z.string(),
+  DB_URL: z.string().url(),
   NODE_ENV: z.enum(['development', 'test', 'production']),
   NEXTAUTH_SECRET: z.string(),
   NEXTAUTH_URL: z.string().url(),

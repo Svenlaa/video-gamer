5093f096aa983185942f1bc69797b4213f1c41dc
Svenlaa <sven.lammertink@hotmail.com>
2022-08-19 17:07:47 +0200

    Add MinIO API !

---
+++ src/pages/file.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next'
+import { trpc } from '../utils/trpc'
+
+const FilePage: NextPage = () => {
+  const buckets = trpc
+    .useQuery(['file.listBuckets'])
+    .data?.Buckets?.map((b) => b.Name)
+  const objects = trpc.useQuery([
+    'file.listObjects',
+    { Bucket: 'buckle-2' }
+  ]).data
+  const fileDate = trpc.useQuery([
+    'file.getObject',
+    { Key: 'test.txt', Bucket: 'buckle-2' }
+  ]).data
+
+  return <p>Hello</p>
+}
+
+export default FilePage
---
+++ src/server/router/file.ts
@@ -0,0 +1,42 @@
+import { createRouter } from './context'
+import { z } from 'zod'
+import { S3 } from 'aws-sdk'
+
+const s3 = new S3({
+  s3ForcePathStyle: true, //! VERY IMPORTANT. DO NOT FORGET THIS
+  endpoint: process.env.S3_ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.S3_ROOT_USER || 'minio',
+    secretAccessKey: process.env.S3_ROOT_PASSWORD || 'password2'
+  }
+})
+
+export const fileRouter = createRouter()
+  .query('listBuckets', {
+    async resolve() {
+      console.log(process.env.MINIO_ROOT_USER)
+      return await s3.listBuckets().promise()
+    }
+  })
+  .query('listObjects', {
+    input: z.object({
+      Bucket: z.string()
+    }),
+    async resolve({ input }) {
+      const { Bucket } = input
+      return await s3.listObjectsV2({ Bucket }).promise()
+    }
+  })
+  .query('getObject', {
+    input: z.object({
+      Key: z.string(),
+      Bucket: z.string()
+    }),
+    resolve: async ({ input }) => {
+      const Key = input.Key
+      const Bucket = input.Bucket
+      return await (
+        await s3.getObject({ Bucket, Key }).promise()
+      ).LastModified
+    }
+  })

711d81238b00451b514b371a9d3ef148c946f66e
Svenlaa <sven.lammertink@hotmail.com>
2022-08-26 15:19:41 +0200

    Improve button accessibility

--- src/components/form/submit.tsx
+++ src/components/form/submit.tsx
@@ -1,15 +1,15 @@
 const SubmitBtn = ({ text }: { text: string }) => {
   return (
-    <div className="group relative inline-block w-full">
-      <button
-        type="submit"
-        className="absolute w-full translate-x-1 translate-y-1 rounded-lg bg-ruby p-4 text-center text-xl font-bold text-gray-300 transition-all duration-500 ease-in-out group-hover:translate-x-0 group-hover:translate-y-0 group-hover:bg-gray-300  group-hover:text-ruby"
+    <div className="group relative inline-block h-16 w-full">
+      <span
+        aria-hidden={true}
+        className="absolute w-full translate-x-1 translate-y-1 rounded-lg bg-ruby p-4 text-center text-xl font-bold text-gray-300 transition-all duration-300 ease-in-out group-hover:translate-x-0 group-hover:translate-y-0 group-hover:bg-gray-300  group-hover:text-ruby"
       >
         {text}
-      </button>
+      </span>
       <button
         type="submit"
-        className="absolute w-full rounded-lg bg-gray-300 p-4 text-xl font-bold text-ruby transition-all duration-500 ease-in-out group-hover:translate-x-1 group-hover:translate-y-1 group-hover:bg-ruby group-hover:text-white"
+        className="absolute w-full rounded-lg bg-gray-300 p-4 text-xl font-bold text-ruby transition-all duration-300 ease-in-out group-hover:translate-x-1 group-hover:translate-y-1 group-hover:bg-ruby group-hover:text-white"
       >
         {text}
       </button>
